37862b6c263f4e89af2aa50dd02f45484644c4af
Daniil Kiselev <kiselev.danil201313@gmail.com>
2021-11-19 15:06:21 +0300

    made lots of stuff

--- interface_t11.py
+++ interface_t11.py
@@ -18,7 +18,7 @@ class Interface:
         master.iconphoto(False, self.photo)  # установка иконки
         master.title('Задача Коши для ОДУ выполнил Варварин Евгений')  # заголовок
         master.configure(bg='#ececec')  # фон
-        master.minsize(1200, 600)  # минимальный размер окна
+        master.minsize(1200, 500)  # минимальный размер окна
 
         self.u0 = tk.DoubleVar(master, 2)  # u0
         self.u0_quote = tk.DoubleVar(master, 3)  # u0'
@@ -34,6 +34,10 @@ class Interface:
         self.cb_var = tk.BooleanVar(master)  # хранит True или False (включен ли контроль погр-ти)
         self.cb_var.set(1)  # значение по умолчанию
 
+        self.label_1 = ['График зависимости скорости U от времени x', 'x, сек.', 'U(x), м/сек.']
+        self.label_2 = ['График зависимости', 'x, сек.', 'U(x), м/сек.']
+        self.label_3 = ['График))', 'x, сек.', 'U(x), м/сек.']
+
         self.create_widgets()
 
     def make_menu(self):
@@ -47,9 +51,25 @@ class Interface:
         mainmenu.add_cascade(label="Параметры задачи", menu=params)
 
         info = Menu(mainmenu, tearoff=0)
-        info.add_command(label="Описание задачи")
-        info.add_command(label="Описание метода")
+        info.add_command(label="Описание задачи", command = self.task_window)
+        info.add_command(label="Описание метода", command = self.method_window)
         mainmenu.add_cascade(label="Информация", menu=info)
+
+    def task_window(self):
+        task = tk.Toplevel()
+        task.title('Описание задачи')
+        img = ImageTk.PhotoImage(Image.open("./task.jpg"))
+        panel = tk.Label(task, image=img)
+        panel.pack(side='bottom', fill='both', expand='yes')
+        task.mainloop()
+
+    def method_window(self):
+        task = tk.Toplevel()
+        task.title('Описание метода')
+        img = ImageTk.PhotoImage(Image.open("./method.jpg"))
+        panel = tk.Label(task, image=img)
+        panel.pack(side='bottom', fill='both', expand='yes')
+        task.mainloop()
 
     def make_frame(self):
         self.frame_top = LabelFrame(text="Параметры")
@@ -130,7 +150,7 @@ class Interface:
 
     def create_notebook(self):
         notebook = ttk.Notebook()
-        notebook.grid(row=0, column=2, sticky = 'n', pady = (10, 0), padx = (10, 10))
+        notebook.grid(row=0, column=2, sticky = 'n', rowspan = 4, pady = (10, 0), padx = (10, 10))
         self.set_nb_frames(notebook)
         notebook.add(self.table_frame, text='Таблица')
         notebook.add(self.graph1_frame, text='График 1')
@@ -139,7 +159,7 @@ class Interface:
         notebook.add(self.info_frame, text='Справка')
 
     def set_nb_frames(self, notebook):
-        self.table_frame = ttk.Frame(notebook, width=900, height=300)
+        self.table_frame = ttk.Frame(notebook, width=900, height=400)
         self.graph1_frame = ttk.Frame(notebook, width=400, height=300)
         self.graph2_frame = ttk.Frame(notebook, width=400, height=300)
         self.graph3_frame = ttk.Frame(notebook, width=400, height=300)
@@ -150,10 +170,136 @@ class Interface:
         self.make_menu()
         self.make_frame()
         self.create_notebook()
-        execute = tk.Button(text='Вычислить', bg='#ececec', highlightbackground='#ececec', command=self.save).grid(
-            row=1, column=1, pady = (10, 10), padx = (10, 0), sticky = 'we')
-        clear = tk.Button(text='Очистить график', bg='#ececec', highlightbackground='#ececec', command=self.save).grid(
-            row=2, column=1, pady = (10, 10), padx = (10, 0), sticky = 'we')
+        execute = tk.Button(text='Вычислить', bg='#ececec', highlightbackground='#ececec', command=self.execute).grid(
+            row=1, column=1, pady = (10, 10), padx = (10, 0), sticky = 'nwe')
+        self.create_table()
+        self.plotOnPlane(self.label_1, self.graph1_frame)
+        self.plotOnPlane(self.label_2, self.graph2_frame)
+        self.plotOnPlane(self.label_3, self.graph3_frame)
+
+    def dll_work(self):
+        init_params = (c_double * 11)()
+        init_params[0] = self.x0.get()
+        init_params[1] = self.u0.get()
+        init_params[2] = 1
+        init_params[3] = self.step.get()
+        init_params[4] = 1
+        init_params[5] = 1
+        init_params[6] = 1
+        init_params[7] = self.error.get()
+        init_params[8] = self.max_step.get()
+        init_params[9] = self.border.get()
+        init_params[10] = self.accuracy.get()
+
+        button_data = (c_int * 2)()
+        button_data[1] = self.cb_var.get()  # контроль ЛП True/False
+
+        dll = cdll.LoadLibrary("dll_for_py2//x64//Release//dll_for_py2.dll")
+        dll.work_RK31R.argtypes = [POINTER(POINTER(c_double))]
+        dll.work_RK31R.restype = None
+        dll.del_mem.argtypes = [POINTER(POINTER(c_double))]
+        dll.work_RK31R.restype = None
+
+        p = {'x': 0, 'V1': 1, 'V2': 2, 'V11': 3, 'V22': 4, 'e': 5, 'h': 6, 'U1': 7, 'U2': 8, 'E': 9, 'c1': 10,
+             'c2': 11, 'k': 12}
+        d = POINTER(c_double)()
+        _i = (c_int)()
+
+        dll.work_RK31R(byref(d), byref(init_params), byref(button_data), byref(_i))
+        return p, d, _i
+
+    def create_table(self):
+        heads = ['k', 'x', 'V1', 'V2', 'V11', 'V22', 'ОЛП', 'h', 'U', 'u2', 'E', 'C1', 'C2']
+        self.table = ttk.Treeview(self.table_frame, show='headings', height=20)
+        self.table['columns'] = heads
+        self.table.grid(row=0, column=0, sticky=tk.NSEW)
+        for header in heads:
+            self.table.heading(header, text=header, anchor='center')
+            self.table.column(header, anchor='center')
+            self.table.column(header, width=70)
+
+    def fill_table(self, p, d, _i):
+        _s = 0
+        for z in range(int(_i.value / p['k'])):
+
+            if d[p['e'] + z * p['k']] == 0:
+                if z != 0:
+                    _s = "<1e-16"
+            else:
+                _s = d[p['e'] + z * p['k']]
+            self.table.insert('', tk.END, values=(
+                z,
+                round((d[p['x'] + z * p['k']]), 4),
+                (d[p['V1'] + z * p['k']]),
+                (d[p['V2'] + z * p['k']]),
+                (d[p['V11'] + z * p['k']]),
+                (d[p['V22'] + z * p['k']]),
+                _s,
+                d[p['h'] + z * p['k']],
+                d[p['U1'] + z * p['k']],
+                d[p['U2'] + z * p['k']],
+                d[p['E'] + z * p['k']],
+                int(d[p['c1'] + z * p['k']]),
+                int(d[p['c2'] + z * p['k']])))
+        scroll_bar1 = Scrollbar(self.table_frame, orient=VERTICAL, command=self.table.yview)
+        scroll_bar1.grid(row=0, column=1, sticky=tk.NSEW)
+        self.table.configure(yscroll=scroll_bar1.set)
+
+    def plotOnPlane(self, label, place, X = [0], Y = [0]):
+        f = plt.figure(figsize=(11, 5), dpi=80, facecolor='#ececec')
+        fig = plt.subplot(1, 1, 1)
+        fig.set_title(label[0])
+        fig.set_xlabel(label[1])
+        fig.set_ylabel(label[2])
+        fig.plot(X, Y, '-k')
+        self.create_form_graph(f, place)
+
+    def create_form_graph(self, figure, place):
+        canvas = FigureCanvasTkAgg(figure, place)
+        canvas.get_tk_widget().grid(row=0, column=0)
+        canvas.draw()
+
+    def fill_graph_1(self, p, d, _i):
+        X = []
+        for z in range(int(_i.value / p['k'])):
+            X.append(d[p['x'] + z * p['k']])
+        Y = []
+        for z in range(int(_i.value / p['k'])):
+            Y.append(d[p['V1'] + z * p['k']])
+        self.plotOnPlane(self.label_1, self.graph1_frame, X, Y)
+
+    def fill_graph_2(self, p, d, _i):
+        X = []
+        for z in range(int(_i.value / p['k'])):
+            X.append(d[p['x'] + z * p['k']])
+        Y = []
+        for z in range(int(_i.value / p['k'])):
+            Y.append(d[p['V1'] + z * p['k']])
+        self.plotOnPlane(self.label_2, self.graph2_frame, X, Y)
+
+    def fill_graph_3(self, p, d, _i):
+        X = []
+        for z in range(int(_i.value / p['k'])):
+            X.append(d[p['x'] + z * p['k']])
+        Y = []
+        for z in range(int(_i.value / p['k'])):
+            Y.append(d[p['V1'] + z * p['k']])
+        self.plotOnPlane(self.label_3, self.graph3_frame, X, Y)
+
+    def reference(self):
+        reference = tk.Label(self.info_frame, text='Тут будет справка', bg='#ececec').grid(row=0, column=0, sticky='w')
+
+    def execute(self):
+        p, d, i = self.dll_work()
+        self.fill_table(p, d, i)
+        self.fill_graph_1(p, d, i)
+        self.fill_graph_2(p, d, i)
+        self.fill_graph_3(p, d, i)
+        self.reference()
+
+
+
+
 
 
 root = tk.Tk()
